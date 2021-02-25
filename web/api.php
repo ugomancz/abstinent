76ff09d02b8db38c68aff7399ddd31cde113bd35
DomcaSuzi <dominio122@gmail.com>
2021-02-26 00:52:12 +0100

    test auto update from git

--- web/api.php
+++ web/api.php
@@ -2,7 +2,7 @@
 $GITLAB_API_KEY= "0183c450e36331faa7cfd704006e0f34";
 $ABSTINENT_API_KEY = "aac69111be3000c6f7608a1924b6f460";
 
-
+// just auto update test
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_SERVER['HTTP_X_GITLAB_TOKEN'])){
         if ($_SERVER['X_GITLAB_TOKEN'] == $GITLAB_API_KEY){
