477048d6c60432771bf06cd64f8c4806050297dc
Ondřej Kostík <xkostik@fi.muni.cz>
2021-02-25 13:35:07 +0100

    POST working with synchronous request

--- include/config.h
+++ include/config.h
@@ -3,7 +3,9 @@
 
 #define SSID "Bulhar01"
 #define PASSWORD "UPC12345bulharska"
-#define SERVER_NAME "abstinent.fun"
+#define SERVER_NAME "https://abstinent.fun/api.php"
+#define SERVER_FOOTPRINT "cf878db671d816387922a9f9b19ffe3e2f485554"
+#define API_KEY "aac69111be3000c6f7608a1924b6f460"
 #define SENSOR 4
 #define SPEED_MEASUREMENT_PERIOD 2
 
